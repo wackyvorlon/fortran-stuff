4ddded6be330ee2cfa934911f525b10774ed3e88
Paul Anderson <paul@andersonloco.com>
2011-03-07 11:15:12 -0500

    Implemented file i/o in foo.f

--- foo.f
+++ foo.f
@@ -3,7 +3,11 @@
 		! implicit none has to do with implicit typing
 		! turns it off
 		real, dimension(100):: a,b,c
-		integer :: i
+		integer :: i, ios
+		open(unit=10, file="bork", iostat=ios, status="new")
+		if ( ios /= 0 ) stop "Error opening bork"
+		
+		
 		! Initialize everything to zero
 		a=0
 		b=0
@@ -17,7 +21,11 @@
 		
 		c=b*a
 		
-		write (*,*) c
+		write (10,*) c
+		!close(unit=10, iostat=ios, status="delete")
+		!if ( ios /= 0 ) stop "Error closing file unit iounit"
+		close(10)
+		
 	
 
 	end program foo
