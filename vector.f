8182fc7dcc074cf24fe7bd8c09185c3989bd7bcd
Paul Anderson <paul@andersonloco.com>
2011-03-08 16:52:40 -0500

    Added variables, code to read in values and output dotproduct.

---
+++ vector.f
@@ -0,0 +1,22 @@
+	program vector
+		implicit none
+		real, dimension(3):: a,b,c
+		real :: d
+		
+		a=0
+		b=0
+		c=0
+		!Say we've got three vectors. Initialized to zero.
+		write (*,*) "Vector a values:"
+		read (*,*) a
+		write(*,*) "Vector b values:"
+		read(*,*) b
+		
+		!Find dot product
+		d = dot_product(a,b)
+		
+		write(*,*) "Dot product: ", d
+		
+
+	end program vector
+	
